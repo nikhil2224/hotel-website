8d135718c33fbaa5a83c901eba458c70987aea6d
NIKHIL CS <nikhilcs526@gmail.com>
2026-04-18 11:03:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/booking")
+def booking():
+    return render_template("booking.html")
+
+@app.route("/cancellation")
+def cancellation():
+    return render_template("cancelation.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
